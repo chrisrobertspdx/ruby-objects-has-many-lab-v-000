9a6b96b3f97161718e92c9a6cc1a68a4f5c9997f
Chris Roberts <chrisrobertspdx@gmail.com>
2017-10-28 22:47:12 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -6,7 +6,11 @@ class Song
   end
 
   def artist_name
-      self.artist.name
+      if self.artist.method_defined? :name
+        self.artist_name
+      else
+        nil
+      end
   end
 
 end
