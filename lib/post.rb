7330722f0dacd92061f968308fe5baa97ad84e1d
Chris Roberts <chrisrobertspdx@gmail.com>
2017-10-28 23:24:12 +0000

    automatically backed up by learn

--- lib/post.rb
+++ lib/post.rb
@@ -1,3 +1,15 @@
 class Post
+  attr_accessor :title
 
+  def initialize(title)
+    @title = title
+  end
+
+  def author_name
+    if self.author != nil
+        self.author.name
+    else
+      nil
+    end
+  end
 end
