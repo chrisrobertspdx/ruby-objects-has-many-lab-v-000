0cc6e2efb83bd48fa12c7b870ceb47bc1abaef5e
Chris Roberts <chrisrobertspdx@gmail.com>
2017-10-28 22:25:32 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -6,6 +6,11 @@ class Artist
     @songs = []
   end
 
+  def add_song(song)
+    @songs << song
+    song.artist = self
+  end
+
   def add_song_by_name(title)
     song = Song.new(title)
     @songs << song
